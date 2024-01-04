eb1dbabdc92877008790ad4a0e076817d62052c0
alfonsojan <alfonsobusker@gmail.com>
2024-01-04 16:32:12 +0100

    Added the data discovery notebook

--- analyses/scripts/create_9_sbs_file.py
+++ analyses/scripts/create_9_sbs_file.py
@@ -1,11 +1,17 @@
 from pathlib import Path
+import re
+import pandas as pd
 from GenomeSigInfer.sbs import SBSMatrixGenerator
 
-_PATH = Path(__file__).parent.parent.parent / "data" / "vcf"
-files = (_PATH / "WES_Other.20180327.simple", _PATH / "WGS_Other.20180413.simple")
-sbs_9_context = SBSMatrixGenerator.generate_single_sbs_matrix("./analyses/SBS/", files, "./analyses/ref_genome/GRCh37", "GRCh37", max_context=9)
-filename = Path(__file__).parent.parent / "SBS" / f"sbs.{sbs_9_context.shape[0]}.parquet"
-sbs_9_context.to_parquet(filename)
+# _PATH = Path(__file__).parent.parent.parent / "data" / "vcf"
+# files = (_PATH / "WES_Other.20180327.simple", _PATH / "WGS_Other.20180413.simple")
+# sbs_9_context = SBSMatrixGenerator.generate_single_sbs_matrix("./analyses/SBS/", files, "./analyses/ref_genome/GRCh37", "GRCh37", max_context=9)
+# filename = Path(__file__).parent.parent / "SBS" / f"sbs.{sbs_9_context.shape[0]}.parquet"
+filename = Path(__file__).parent.parent / "SBS" / f"sbs.393216.parquet"
+filename = Path(__file__).parent.parent / "SBS" / "s.parquet"
+sbs_9_context = pd.read_parquet(filename)
+print(sbs_9_context)
+# sbs_9_context.to_parquet(filename)
 
 # NUCL_MAP = {
 #     "nucl_strength": {
@@ -19,22 +25,16 @@ sbs_9_context.to_parquet(filename)
 #     }
 # }
 
-# def replace_nucleotides(df, cluster_type, columns):
-#     """
-#     Replace nucleotides in specific columns of the DataFrame.
+NUCL_MAP = {
+    "nucl_strength": {"A": "W", "T": "W", "C": "S", "G": "S"},
+    "amino": {"A": "M", "C": "M", "G": "K", "T": "K"},
+    "structure": {"A": "R", "C": "Y", "G": "R", "T": "Y"}
+}
 
-#     Args:
-#         df (pandas.DataFrame): The DataFrame to modify.
-#     """
-#     for col in columns:
-#         for key, value in cluster_type.items():
-#             df.loc[df[col].isin(value), col] = key
+def cluster(df, cluster_type, filename):
+    df['MutationType'] = df['MutationType'].apply(lambda x: ''.join(cluster_type.get(c, c) for c in x[:2]) + x[2:-2] + ''.join(cluster_type.get(c, c) for c in x[-2:]))
+    df = df.groupby("MutationType").sum()
+    print(df)
 
-# def cluster(df):
-#     col_index = {"-4": 0, "-3": 1, "3": 11, "4": 12}
-#     for col, index in col_index.items():
-#         df[col] = df["MutationType"].str[index]
-#     replace_nucleotides(df, NUCL_MAP["nucl_strength"], col_index.keys())
-#     df["MutationType"] = df["-4"] + df["-3"] + df["MutationType"].str[2:11] + df["3"] + df["4"]
-#     df.drop(["-4", "-3", "3", "4"], axis=1, inplace=True)
-#     print(df.groupby("MutationType").sum())
+
+cluster(sbs_9_context, NUCL_MAP["nucl_strength"])
